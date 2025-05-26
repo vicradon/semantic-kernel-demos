8a88511d335d96b5e4a0c4d807bd4a20d4568e44
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2025-05-26 08:43:55 +0000

    chore: rearrange the github

---
+++ basic-prompt/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SemanticKernel;
+
+var modelId = "gpt-4o-mini";
+var endpoint = "https://openai202.openai.azure.com";
+var apiKey = "";
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+Kernel kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync("Give me a list of breakfast foods with eggs and cheese");
+Console.WriteLine(result);
+
+
---
+++ intent-investigation/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+
+var modelId = "gpt-4o-mini";
+var endpoint = "https://openai202.openai.azure.com";
+var apiKey = "";
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+Kernel kernel = builder.Build();
+
+
+string request = "I want to make a pdf";
+string prompt = $"""
+Instructions: What is the intent of this request?
+If you don't know the intent, don't guess; instead respond with "Unknown".
+Choices: SendEmail, SendMessage, CompleteTask, CreateDocument, Unknown.
+User Input: {request}
+Intent: 
+""";
+
+var intentFunction = kernel.CreateFunctionFromPrompt(prompt);
+var result = await intentFunction.InvokeAsync(kernel);
+Console.WriteLine(result);
+
